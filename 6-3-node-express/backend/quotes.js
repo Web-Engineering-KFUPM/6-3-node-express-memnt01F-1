3437cf1cb731bb8ad582ed1b3cf64074487b5af0
Reem <s202360310@kfupm.edu.sa>
2026-04-06 13:14:18 +0300

    todo3

--- 6-3-node-express/backend/quotes.js
+++ 6-3-node-express/backend/quotes.js
@@ -1,11 +1,5 @@
 // Import random.js file
 
-const quotes = [
-  "Code is like humor. When you have to explain it, it’s bad.",
-  "Before software can be reusable it first has to be usable.",
-  "Make it work, make it right, make it fast.",
-  "Programming isn’t about what you know; it’s about what you can figure out."
-];
 
 // TODO 3: Create and export getRandomQuote function
 //Hint: export function getRandomQuote(){}
@@ -23,4 +17,3 @@ export function getRandomQuote() {
   const index = getRandomInt(quotes.length);
   return quotes[index];
 }
-

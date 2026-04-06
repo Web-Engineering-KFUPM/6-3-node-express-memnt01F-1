e81aafa83d9d208423f98f1d761efb0893a89d78
Reem <s202360310@kfupm.edu.sa>
2026-04-06 12:56:12 +0300

    todo3

--- 6-3-node-express/backend/quotes.js
+++ 6-3-node-express/backend/quotes.js
@@ -9,4 +9,18 @@ const quotes = [
 
 // TODO 3: Create and export getRandomQuote function
 //Hint: export function getRandomQuote(){}
+import { getRandomInt } from "./utils/random.js";
+
+const quotes = [
+  "Believe in yourself.",
+  "Success is built one step at a time.",
+  "Small progress is still progress.",
+  "Discipline beats motivation.",
+  "Start now, improve later."
+];
+
+export function getRandomQuote() {
+  const index = getRandomInt(quotes.length);
+  return quotes[index];
+}
 

bb9310ff3b8bd7e9cbbf4a500980b3c86d69af2f
Reem <s202360310@kfupm.edu.sa>
2026-04-06 13:15:11 +0300

    todo1-6

--- 6-3-node-express/backend/server.js
+++ 6-3-node-express/backend/server.js
@@ -1,36 +1,60 @@
-// TODO 1: Import and Create express app instance
+// // TODO 1: Import and Create express app instance
 
-// TODO 1: Define server port
+// // TODO 1: Define server port
+// import express from "express";
+
+// const app = express();
+// const PORT = 3000;
+
+
+// // TODO 4: Import and Apply CORS middleware
+// import cors from "cors";
+// app.use(cors());
+
+
+// // TODO 5: Import and apply Morgan middleware
+// import morgan from "morgan";
+// app.use(morgan("dev"));
+
+
+// // TODO 6.1: Create root route "/"
+// app.get("/", (req, res) => {
+//   res.send("Backend server is running");
+// });
+
+// // TODO 6.2: Create "/api/quote" route
+// import { getRandomQuote } from "./quotes.js";
+// app.get("/api/quote", (req, res) => {
+//   const quote = getRandomQuote();
+//   res.json({ quote });
+// });
+
+
+// // TODO 7: Start server using app.listen
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
+
 import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import { getRandomQuote } from "./quotes.js";
 
 const app = express();
 const PORT = 3000;
 
-
-// TODO 4: Import and Apply CORS middleware
-import cors from "cors";
 app.use(cors());
-
-
-// TODO 5: Import and apply Morgan middleware
-import morgan from "morgan";
 app.use(morgan("dev"));
 
-
-// TODO 6.1: Create root route "/"
 app.get("/", (req, res) => {
   res.send("Backend server is running");
 });
 
-// TODO 6.2: Create "/api/quote" route
-import { getRandomQuote } from "./quotes.js";
 app.get("/api/quote", (req, res) => {
   const quote = getRandomQuote();
   res.json({ quote });
 });
 
-
-// TODO 7: Start server using app.listen
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
